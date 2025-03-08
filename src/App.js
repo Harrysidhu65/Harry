7d9a262996bc8a92764c55dfa8f09dfadd6605d1
hardeep singh <sidhu.harry76@gmail.com>
2025-03-08 12:53:30 +0530

    app change

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 
 import Home from "./components/home";
+
+import {Route, Routes, HashRouter } from 'react-router-dom';
 import React from 'react';
 import Layout from "./components/layout";
 import About from "./components/about";
 import Service from "./components/service";
 import Contact from "./components/contact";
-import { HashRouter as Route, Routes } from "react-router-dom";
 
 function App() {
   
