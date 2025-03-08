2d86f3db6b0c68d4f8a97ce47519564cfc49add4
hardeep singh <sidhu.harry76@gmail.com>
2025-03-08 12:56:20 +0530

    app final changes

--- src/App.js
+++ src/App.js
@@ -4,9 +4,6 @@ import Home from "./components/home";
 import {Route, Routes, HashRouter } from 'react-router-dom';
 import React from 'react';
 import Layout from "./components/layout";
-import About from "./components/about";
-import Service from "./components/service";
-import Contact from "./components/contact";
 
 function App() {
   
@@ -16,9 +13,6 @@ function App() {
         <Routes>
            <Route path="/" element={<Layout/>}>
             <Route index element={<Home />} />
-            <Route path="/about" element={<About />} />
-            <Route path="/service" element={<Service />} />
-            <Route path="/contact" element={<Contact/>} />
            </Route>
       </Routes>
     </HashRouter>
